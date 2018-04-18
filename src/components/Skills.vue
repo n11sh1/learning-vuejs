87b62fe80710e85e36a68034c2bd3e9549e965cd
Niishi Kubo <214@niishi.local>
2018-04-19 01:51:39 +0900

    first step

---
+++ src/components/Skills.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+      </ul>
+
+      <div v-bind:class="alertObject"></div>
+      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
+
+      <p>There are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+      return {
+          skills: [
+              { "skill": "Vue.js" },
+              { "skill": "Frontend Developer" }
+          ],
+          alertObject: {
+              alert: true
+          },
+          bgColor: 'blue',
+          bgWidth: '100%',
+          bgHeight: '30px'
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .alert {
+    background-color: yellow;
+    width: 100%;
+    height: 30px;
+  }
+  .another-class {
+    border: 5px solid black;
+  }
+</style>
